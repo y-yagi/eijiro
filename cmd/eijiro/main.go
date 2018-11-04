248cc4750c1a6b26607d6e792ee12c6224f5b46e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-11-04 13:21:40 +0900

    First commit

---
+++ cmd/eijiro/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/y-yagi/configure"
+	"github.com/y-yagi/eijiro"
+)
+
+const cmd = "eijiro"
+
+type config struct {
+	DataBase string `toml:"database"`
+}
+
+var cfg config
+
+func init() {
+	err := configure.Load(cmd, &cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(cfg.DataBase) == 0 {
+		cfg.DataBase = filepath.Join(configure.ConfigDir(cmd), cmd+".db")
+		configure.Save(cmd, cfg)
+	}
+}
+
+func main() {
+	os.Exit(run(os.Args, os.Stdout, os.Stderr))
+}
+
+func run(args []string, outStream, errStream io.Writer) (exitCode int) {
+	var importFile string
+	var config bool
+
+	exitCode = 0
+
+	flags := flag.NewFlagSet(cmd, flag.ExitOnError)
+	flags.SetOutput(errStream)
+	flags.StringVar(&importFile, "i", "", "Import file.")
+	flags.BoolVar(&config, "c", false, "Edit config.")
+	flags.Parse(args[1:])
+
+	if config {
+		editor := os.Getenv("EDITOR")
+		if len(editor) == 0 {
+			editor = "vim"
+		}
+
+		if err := configure.Edit(cmd, editor); err != nil {
+			fmt.Fprintf(errStream, "Error: %v\n", err)
+			exitCode = 1
+			return
+		}
+		return
+	}
+
+	ej := eijiro.NewEijiro(cfg.DataBase)
+	err := ej.InitDB()
+	if err != nil {
+		fmt.Fprintf(errStream, "Error: %v\n", err)
+		exitCode = 1
+		return
+	}
+
+	if len(importFile) != 0 {
+		if err = ej.Import(importFile); err != nil {
+			fmt.Fprintf(errStream, "Error: %v\n", err)
+			exitCode = 1
+			return
+		}
+		return
+	}
+
+	if len(flags.Args()) != 1 {
+		exitCode = 2
+		usage(errStream)
+		return
+	}
+
+	documents, err := ej.Select(flags.Args()[0])
+	if err != nil {
+		fmt.Fprintf(errStream, "Error: %v\n", err)
+		exitCode = 1
+		return
+	}
+
+	for _, document := range documents {
+		fmt.Fprintf(outStream, "%s\n", document.Text)
+	}
+
+	return
+}
+
+func usage(errStream io.Writer) {
+	fmt.Fprintf(errStream, "Usage: %s [OPTIONS] text\n", cmd)
+}
---
+++ eijiro.go
@@ -0,0 +1,103 @@
+package eijiro
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/y-yagi/goext/osext"
+)
+
+var (
+	schema = `
+CREATE TABLE documents (
+	id integer primary key autoincrement not null,
+	text varchar not null
+);
+`
+	insertQuery = `
+INSERT INTO documents (text) VALUES ($1)
+`
+
+	selectQuery = `
+SELECT text FROM documents WHERE text LIKE $1
+`
+)
+
+// Eijiro is a eijiro module.
+type Eijiro struct {
+	database string
+}
+
+// Document is type for `dictionaries` table
+type Document struct {
+	ID   int    `db:"id"`
+	Text string `db:"text"`
+}
+
+// NewEijiro creates a new eijiro.
+func NewEijiro(database string) *Eijiro {
+	eijiro := &Eijiro{database: database}
+	return eijiro
+}
+
+// InitDB initialize database.
+func (e *Eijiro) InitDB() error {
+	if osext.IsExist(e.database) {
+		return nil
+	}
+
+	db, err := sqlx.Connect("sqlite3", e.database)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	db.MustExec(schema)
+
+	return nil
+}
+
+// Import file to database
+func (e *Eijiro) Import(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	db, err := sqlx.Connect("sqlite3", e.database)
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(file)
+	tx := db.MustBegin()
+	for scanner.Scan() {
+		tx.MustExec(insertQuery, scanner.Text())
+	}
+
+	if err = scanner.Err(); err != nil {
+		return err
+	}
+
+	tx.Commit()
+	return nil
+}
+
+// Select select text from database
+func (w *Eijiro) Select(search string) ([]Document, error) {
+	db, err := sqlx.Connect("sqlite3", w.database)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	documents := []Document{}
+	err = db.Select(&documents, selectQuery, "%"+search+"%")
+	if err != nil {
+		return nil, err
+	}
+
+	return documents, nil
+}
